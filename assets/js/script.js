b1107c40e9693b6b69fd81f2b9dd685498d52d78
Rodrigo Miranda <rodrigomiranda023@gmail.com>
2023-04-29 11:55:51 -0400

    replaceAll e interpolacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-console.log('Hola soy botman')
+var btnEnviar = document.querySelector('#btnEnviar');
+
+btnEnviar.addEventListener('click', function(){
+    var cajaUno = document.querySelector('#cajaUno');
+    var cajaDos = document.querySelector('#cajaDos');
+    var chxUno = document.querySelector('#chxUno');
+
+    /* replaceAll - trimStart - trimEnd */
+
+    console.log(cajaUno.value)
+    console.log(cajaDos.value);
+
+    /* replaceAll */
+/* cambiar 'esto' por 'con esta cadena de caracteres jugaremos' */
+/*   var textoNuevo = cajaUno.value.replaceAll('esto', 'esta cadena de caracteres');
+    console.log(textoNuevo);
+    cajaUno.value = textoNuevo;
+    console.log(cajaUno.value) */
+    var nombre = prompt('ingresa tu nombre');
+    cajaUno.value = cajaUno.value.replaceAll('esto', `esta cadena de caracteres ${nombre}`);
+    console.log(cajaUno.value);
+
+
+    /* Interpolacion */
+    /* Al apretar el boton pediremos el nomrbe mediante un prompt y
+   dicho nombre hay que agretgarlo a la cadena actualizada */
+
+});
+
+/* template string - back ticks */
